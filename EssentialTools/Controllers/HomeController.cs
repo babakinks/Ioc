65905c9c60ab6b70f68630d7d1efe9cce2895ad8
Babak Aghili <babakaghili@icloud.com>
2014-06-21 23:41:28 -0500

    added ninject

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 257M},
+            new Product {Name = "Lifejacket", Category = "Lifejacket", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.5M},
+            new Product {Name = "Corner Flag", Category = "Soccer", Price = 35.4M}
+        };
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueConverter>().To<LinqValueCalculator>();
+
+            //IValueConverter calc = new LinqValueCalculator();
+            IValueConverter calc = ninjectKernel.Get<IValueConverter>();
+
+            ShoppingCart cart = new ShoppingCart(calc) {Products = products};
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+
+    }
+}
---
+++ EssentialTools/Models/IValueConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public interface IValueConverter
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+}
